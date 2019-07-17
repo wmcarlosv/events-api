3e67151afc404e02b80facc9ccd5e03f853fc85a
wmcarlosv <=>
2019-07-17 10:10:33 -0400

    arreglando algunos campos de la base de datos

--- src/database/events-admin.sql
+++ src/database/events-admin.sql
@@ -4,8 +4,8 @@ CREATE TABLE users(
 	email varchar(255) not null,
 	password varchar(64) not null,
 	role enum('administrator','operator'),
-	create_at timestamp not null default current_timestamp,
-	update_at timestamp null,
+	created_at timestamp not null default current_timestamp,
+	updated_at timestamp null,
 	primary key (id)
 )engine = InnoDB;
 
@@ -18,7 +18,7 @@ CREATE TABLE envents(
 	cover varchar(100) null,
 	envent_date timestamp,
 	status enum('pending','executed'),
-	created_ad timestamp not null default current_timestamp,
+	created_at timestamp not null default current_timestamp,
 	updated_at timestamp null,
 	primary key (id),
 	foreign key (user_id) references users(id)
